878d97848f148f5d9db601133508f2cd1c01e5f9
Likithchilakuri <86821628+Likithchilakuri@users.noreply.github.com>
2026-08-01 12:25:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import { sendTelegram } from './telegram.js';
-
+import { startMonitor } from "./monitor.js";
 console.log("🚀 X Keyword Monitor Started");
 
 try {
   await sendTelegram("✅ X Keyword Monitor is now online!");
   console.log("Telegram test message sent.");
+  await startMonitor();
 } catch (err) {
   console.error("Telegram Error:", err.message);
 }
